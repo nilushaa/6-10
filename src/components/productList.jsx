b203ed0666cf79a2f0e0538fafaa358d0531db01
nilufar <nilufar@gmail.com>
2025-07-08 13:43:29 +0500

    n

---
+++ src/components/productList.jsx
@@ -0,0 +1,33 @@
+import Product from "./product";
+import { useGlobalContext } from "../hooks/useGlobalContext";
+
+function ProductsList({ products }) {
+  const { totalPrice, dispatch } = useGlobalContext();
+  return (
+    <div className="card-container">
+      <div className="card-container__header">
+        <p className="card-container__title">Product List: </p>
+        <div>
+          <span className="card-container__price">
+            Total Price: ${totalPrice}
+          </span>
+          <button
+            onClick={() =>
+              dispatch({
+                type: "CLEAR",
+              })
+            }
+            className="btn card-container__btn"
+          >
+            Clear
+          </button>
+        </div>
+      </div>
+      {products.map((product) => {
+        return <Product key={product.id} product={product} />;
+      })}
+    </div>
+  );
+}
+
+export default ProductsList;
